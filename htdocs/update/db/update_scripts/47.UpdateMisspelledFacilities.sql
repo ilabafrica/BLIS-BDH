9d1940f4a5d7d63871c47c143170cd5f2bcca476
Brian Maiyo <kipropbrian@gmail.com>
2014-11-21 18:30:00 +0300

    Scripts for #78

---
+++ htdocs/update/db/update_scripts/47.UpdateMisspelledFacilities.sql
@@ -0,0 +1,138 @@
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BGM CCC'; 
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgm lab'; 
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bungoma';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgm';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BGM/CCC';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BGMLAB';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgm cc';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgm lab';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgmk ccc';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bvgm ccc';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BGM-CCC';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BGM ANC';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgm cccc';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = ' BDHL';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BGM-LAB';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgmccc';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'BUNGOMA LAB';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'mch lab';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bgm ccc lab';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'bmg ccc';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = ' bgm lab';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'self';
+update blis_301.specimen_custom_data set field_value = 'BUNGOMA DISTRICT HOSPITAL CCC' where field_value = 'ccc';
+
+
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'Kemri Alupe';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'Alupe Kemri';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'kemri-alupe';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'alupe';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'alupe-kemri';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'ALUPE  KEMRI';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'alupe lab';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'kmri alupe';
+update blis_301.specimen_custom_data set field_value = 'KEMRI ALUPE' where field_value = 'kemri lab';
+
+update blis_301.specimen_custom_data set field_value = 'CHWELE SUB COUNTY HOSPITAL' where field_value = 'chwele';
+update blis_301.specimen_custom_data set field_value = 'CHWELE SUB COUNTY HOSPITAL' where field_value = 'Chwele ccc';
+update blis_301.specimen_custom_data set field_value = 'CHWELE SUB COUNTY HOSPITAL' where field_value = 'Cwele ccc';
+update blis_301.specimen_custom_data set field_value = 'CHWELE SUB COUNTY HOSPITAL' where field_value = 'Chwle ccc';
+update blis_301.specimen_custom_data set field_value = 'CHWELE SUB COUNTY HOSPITAL' where field_value = 'CHWELE SUB DISTRICT';
+
+update blis_301.specimen_custom_data set field_value = 'KABULA DISPENSARY' where field_value = 'KABULA';
+update blis_301.specimen_custom_data set field_value = 'KABULA DISPENSARY' where field_value = 'KABULA H/C';
+
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'st domiano';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'st domian';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'st. damiano';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'ST DAMIANO';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'ST DOMIANO M.C';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'st.damiano';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'st domiano mc';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'ST DOMIANO M .C';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'ST.DAMIANO M C';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'st dominian';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'ST. DAMIANO M C';
+update blis_301.specimen_custom_data set field_value = 'ST.DAMIANO MEDICAL CENTRE' where field_value = 'SDMC';
+
+update blis_301.specimen_custom_data set field_value = 'BUMULA HEALTH CENTRE' where field_value = 'bumula';
+update blis_301.specimen_custom_data set field_value = 'BUMULA HEALTH CENTRE' where field_value = 'Bumula ccc';
+
+update blis_301.specimen_custom_data set field_value = 'SIBOTI MODEL HEALTH CENTRE' where field_value = 'siboti model';
+update blis_301.specimen_custom_data set field_value = 'SIBOTI MODEL HEALTH CENTRE' where field_value = 'siboti';
+update blis_301.specimen_custom_data set field_value = 'SIBOTI MODEL HEALTH CENTRE' where field_value = 'siboti model H/F';
+update blis_301.specimen_custom_data set field_value = 'SIBOTI MODEL HEALTH CENTRE' where field_value = 'siboti h/c';
+update blis_301.specimen_custom_data set field_value = 'SIBOTI MODEL HEALTH CENTRE' where field_value = 'Saboti model H/F';
+
+
+update blis_301.specimen_custom_data set field_value = 'ELGON VIEW MEDICAL COTTAGE' where field_value = 'Elgon View';
+update blis_301.specimen_custom_data set field_value = 'ELGON VIEW MEDICAL COTTAGE' where field_value = 'Elgon view medical ';
+update blis_301.specimen_custom_data set field_value = 'ELGON VIEW MEDICAL COTTAGE' where field_value = 'ELGON VIEW M C';
+update blis_301.specimen_custom_data set field_value = 'ELGON VIEW MEDICAL COTTAGE' where field_value = 'ELGON VIEW MEDICAL CUTTAGE';
+update blis_301.specimen_custom_data set field_value = 'ELGON VIEW MEDICAL COTTAGE' where field_value = 'ELGON VIEW  M .C';
+
+update blis_301.specimen_custom_data set field_value = 'KIBUKE DISPENSARY' where field_value = 'kibuke';
+update blis_301.specimen_custom_data set field_value = 'KIBUKE DISPENSARY' where field_value = 'kibuke dsp';
+
+update blis_301.specimen_custom_data set field_value = 'BULONDO DISPENSARY' where field_value = 'bulondo';
+update blis_301.specimen_custom_data set field_value = 'BULONDO DISPENSARY' where field_value = 'bulondo dispensary';
+update blis_301.specimen_custom_data set field_value = 'BULONDO DISPENSARY' where field_value = 'Bulondo Despensary';
+update blis_301.specimen_custom_data set field_value = 'BULONDO DISPENSARY' where field_value = 'BULONDO DISPENCARY';
+
+update blis_301.specimen_custom_data set field_value = 'NZOIA SUGAR DISPENSARY' where field_value = 'nzoia sugar clinic';
+update blis_301.specimen_custom_data set field_value = 'NZOIA SUGAR DISPENSARY' where field_value = 'nzoia sugar company';
+update blis_301.specimen_custom_data set field_value = 'NZOIA SUGAR DISPENSARY' where field_value = 'Nzoia sugar';
+update blis_301.specimen_custom_data set field_value = 'NZOIA SUGAR DISPENSARY' where field_value = 'NZOIA SUGAR C';
+update blis_301.specimen_custom_data set field_value = 'NZOIA SUGAR DISPENSARY' where field_value = 'nzoia';
+
+update blis_301.specimen_custom_data set field_value = 'MILUKI DISPENSARY' where field_value = 'miluki';
+update blis_301.specimen_custom_data set field_value = 'MILUKI DISPENSARY' where field_value = 'MILUKI DISP';
+update blis_301.specimen_custom_data set field_value = 'MILUKI DISPENSARY' where field_value = 'miluki disp.';
+update blis_301.specimen_custom_data set field_value = 'MILUKI DISPENSARY' where field_value = 'miluki Despensary';
+update blis_301.specimen_custom_data set field_value = 'MILUKI DISPENSARY' where field_value = 'miluki dsp';
+
+update blis_301.specimen_custom_data set field_value = 'MACHWELE DISPENSARY' where field_value = 'machwele dispensary';
+update blis_301.specimen_custom_data set field_value = 'MACHWELE DISPENSARY' where field_value = 'machwele';
+update blis_301.specimen_custom_data set field_value = 'MACHWELE DISPENSARY' where field_value = 'machwele despensary';
+
+update blis_301.specimen_custom_data set field_value = 'AKUH NAIROBI' where field_value = 'AKUH';
+update blis_301.specimen_custom_data set field_value = 'AKUH NAIROBI' where field_value = 'AKUH - NAIROBI';
+update blis_301.specimen_custom_data set field_value = 'AKUH NAIROBI' where field_value = 'AKUH- NAIROBI';
+
+update blis_301.specimen_custom_data set field_value = 'KABUCHAI HEALTH CENTRE' where field_value = 'KABUCHAI H/C';
+update blis_301.specimen_custom_data set field_value = 'KABUCHAI HEALTH CENTRE' where field_value = 'KABUCHAI';
+update blis_301.specimen_custom_data set field_value = 'KABUCHAI HEALTH CENTRE' where field_value = 'Kabuichai H/C';
+
+update blis_301.specimen_custom_data set field_value = 'KIMAETI DISPENSARY' where field_value = 'kimaeti despensary';
+update blis_301.specimen_custom_data set field_value = 'KIMAETI DISPENSARY' where field_value = 'kimaeti H/C';
+update blis_301.specimen_custom_data set field_value = 'KIMAETI DISPENSARY' where field_value = 'kimaeti';
+update blis_301.specimen_custom_data set field_value = 'KIMAETI DISPENSARY' where field_value = 'kimaet';
+update blis_301.specimen_custom_data set field_value = 'KIMAETI DISPENSARY' where field_value = 'kimaeti ccc';
+
+update blis_301.specimen_custom_data set field_value = 'NALONDO MODEL HEALTH CENTRE' where field_value = 'Nalondo Model H/C';
+update blis_301.specimen_custom_data set field_value = 'NALONDO MODEL HEALTH CENTRE' where field_value = 'nalondo';
+update blis_301.specimen_custom_data set field_value = 'NALONDO MODEL HEALTH CENTRE' where field_value = 'NALONDO H/C';
+update blis_301.specimen_custom_data set field_value = 'NALONDO MODEL HEALTH CENTRE' where field_value = 'Nalando';
+
+update blis_301.specimen_custom_data set field_value = 'LUUYA DISPENSARY' where field_value = 'Luuya Dsp';
+update blis_301.specimen_custom_data set field_value = 'LUUYA DISPENSARY' where field_value = 'LUUYA DISP';
+
+update blis_301.specimen_custom_data set field_value = 'EKITALE DISPENSARY' where field_value = 'EKITALE';
+update blis_301.specimen_custom_data set field_value = 'EKITALE DISPENSARY' where field_value = 'Ekitale Dsp';
+
+update blis_301.specimen_custom_data set field_value = 'MUMBULE DISPENSARY' where field_value = 'MUMBULE DISP';
+
+update blis_301.specimen_custom_data set field_value = 'NASIANDA DISPENSARY' where field_value = 'NASINDA';
+
+update blis_301.specimen_custom_data set field_value = 'AGA KHAN UNIVERSITY HOSPITAL' where field_value = 'AKHAN UNIVERSITY HOSPITAL LABORATORY';
+
+update blis_301.specimen_custom_data set field_value = 'LUMBOKA MEDICAL SERVICES' where field_value = 'lumboka';
+
+update blis_301.specimen_custom_data set field_value = 'GK PRISON DISPENSARY, BUNGOMA SOUTH' where field_value = 'GK-PRISONS';
+update blis_301.specimen_custom_data set field_value = 'GK PRISON DISPENSARY, BUNGOMA SOUTH' where field_value = 'GK';
+update blis_301.specimen_custom_data set field_value = 'GK PRISON DISPENSARY, BUNGOMA SOUTH' where field_value = 'GK PRISONS';
+update blis_301.specimen_custom_data set field_value = 'GK PRISON DISPENSARY, BUNGOMA SOUTH' where field_value = 'bgm prison';
+
+update blis_301.specimen_custom_data set field_value = 'KITALE DISTRICT HOSPITAL' where field_value = 'kitale District Hospital';
+
+update blis_301.specimen_custom_data set field_value = 'CHEBUKAKA MISSION HOSPITAL' where field_value = 'chebukaka dsp';
